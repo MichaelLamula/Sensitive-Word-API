e75b84d5d0a6a36706fdd9cd5b125f3799a5474c
michaellamula <micheallamula@gmail.com>
2026-06-25 02:51:45 +0200

    chore:remove MSSQL configs.

--- src/test/java/Sensitive/Words/sensitve_words/SensitveWordsApplicationTests.java
+++ src/test/java/Sensitive/Words/sensitve_words/SensitveWordsApplicationTests.java
@@ -1,17 +1,9 @@
 package Sensitive.Words.sensitve_words;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ComponentScan(basePackages = {"Sensitive.Words.sensitve_words", "security", "service", "controller", "config"})
-@EntityScan(basePackages = {"entity"})
-@EnableJpaRepositories(basePackages = {"repository"})
-@ActiveProfiles("test")
 class SensitveWordsApplicationTests {
 
 	@Test
